a2c9ec0ae92ad757f481dd746843970caabbdd88
Sofasmile <sofiya.dyakonova@ukr.net>
2019-06-12 18:27:20 +0300

    binary string

---
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class BinaryString {
+
+    /**
+     * Напишите метод toBinaryString(), который принимает на вход целое число value,
+     * а возвращает String с представлением этого числа в двоичном виде.
+     */
+    public String toBinaryString(int value) {
+        return null;
+    }
+}
