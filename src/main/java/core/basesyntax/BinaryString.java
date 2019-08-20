4c0b07883edd5d48b725d45b3d1a7acdb388cc7e
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-08-20 15:44:49 +0300

    fixed method toBinaryString()

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -3,11 +3,20 @@ package core.basesyntax;
 public class BinaryString {
 
     /**
-     * Напишите метод toBinaryString(), который принимает на вход целое число value,
-     * а возвращает String с представлением этого числа в двоичном виде.
+     * Напишите метод toBinaryString(), который принимает на вход целое число value, а возвращает
+     * String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
+        StringBuilder builder = new StringBuilder();
 
-        return Integer.toBinaryString(value);
+        if (value == 0) {
+            return "0";
+        }
+
+        while (value > 0) {
+            builder.append(value % 2);
+            value /= 2;
+        }
+        return builder.reverse().toString();
     }
 }
