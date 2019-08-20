9747d5e040aad65c43cf45439cbc78799b88d5dd
Korfat <38156885+Korfat@users.noreply.github.com>
2019-08-20 15:10:53 +0300

    added solution value to binary

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,15 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        String result = "";
+        String reverse = "";
+        do {
+            reverse += value % 2;
+            value /= 2;
+        } while (value >= 1);
+        for (int i = reverse.length() - 1; i >= 0; i--) {
+            result += reverse.charAt(i);
+        }
+        return result;
     }
 }
