fa65cb72f267d9e1323271091609d22513d8bc3a
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-20 13:21:25 +0300

    toBinaryString method finished in BinaryString class

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,14 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        if (value == 0) {
+            return "0";
+        }
+        StringBuilder binaryRes = new StringBuilder();
+        while (value > 0) {
+            binaryRes.append(value % 2);
+            value /= 2;
+        }
+        return binaryRes.reverse().toString();
     }
 }
