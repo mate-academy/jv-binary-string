f52c31e8ed77765178b31ab1f0a7b710512f4d9b
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-20 15:01:10 +0300

    added solution for toBinaryString homework

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,29 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return Integer.toBinaryString(value);
+        //looking for root of maximal bit of value
+        int root = 0;
+        int tmp = 1;
+        while (tmp * 2 <= value) {
+            tmp *= 2;
+            root++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        tmp = value;
+        for (int i = root; i > 0; i--) {
+            if (Math.pow(2, i) <= tmp) {
+                result.append("1");
+                tmp -= Math.pow(2, i);
+            } else {
+                result.append("0");
+            }
+        }
+        if (value % 2 == 0) {
+            result.append("0");
+        } else {
+            result.append("1");
+        }
+        return result.toString();
     }
 }
