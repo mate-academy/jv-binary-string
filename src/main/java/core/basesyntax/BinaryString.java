1d34f8349f6a8113c335e61ab0b80ad2e2c97a09
lenovo <suleimanovayev@gmail.com>
2019-08-21 06:13:14 +0300

    add sol by binary

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,14 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder str = new StringBuilder();
+        if (value == 0) {
+            str.append(0);
+        }
+        while (value != 0) {
+            str.append(String.valueOf(value % 2).concat(""));
+            value /= 2;
+        }
+        return str.reverse().toString();
     }
 }
