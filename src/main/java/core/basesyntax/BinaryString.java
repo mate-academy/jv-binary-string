2562e98de09640998a6e3ccf82a984cf50b20cab
Vadim Chaika <proxik777@gmail.com>
2019-12-02 14:05:06 +0200

    added a solution for binary string homework

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,11 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        String result = "";
+        while (value !=0) {
+            result += String.valueOf(value%2);
+            value = value / 2;
+        }
+        return result;
     }
 }
