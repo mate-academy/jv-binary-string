94f8c8cfd29a27272af2d597ef8ff19fd0cfb71e
anyapress777@gmail.com <golovinzodchiy95>
2019-12-02 21:00:02 +0200

    task 1.3 was finished

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,15 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        if (value == 0) {
+            return "0";
+        } else {
+            StringBuilder result = new StringBuilder();
+            while (value != 0) {
+                result.append(value % 2);
+                value = value / 2;
+            }
+            return result.reverse().toString();
+        }
     }
 }
