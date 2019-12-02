544eaebb3660a7fa27b4302d6663c427f7d1e356
Riptod <otoval15>
2019-12-02 09:05:10 +0200

    hw-1.3

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,11 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder binary = new StringBuilder();
+        do {
+            binary.append(value % 2);
+            value /= 2;
+        } while (value >= 1);
+        return String.valueOf(binary.reverse());
     }
 }
