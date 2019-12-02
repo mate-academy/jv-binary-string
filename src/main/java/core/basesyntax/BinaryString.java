0d3591a6f9418069cbb874d42627a16929e90edb
Riptod <otoval15>
2019-12-02 14:27:07 +0200

    fixed mistakes

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -12,6 +12,6 @@ public class BinaryString {
             binary.append(value % 2);
             value /= 2;
         } while (value >= 1);
-        return String.valueOf(binary.reverse());
+        return binary.reverse().toString();;
     }
 }
