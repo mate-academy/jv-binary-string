4060ed26c69d9634a143a5e4918bd56cd4b0cfb8
Julianevinska <57527101+Julianevinska@users.noreply.github.com>
2019-12-03 01:42:47 +0200

    Homework 1.3 - solution

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -6,7 +6,22 @@ public class BinaryString {
      * Напишите метод toBinaryString(), который принимает на вход целое число value,
      * а возвращает String с представлением этого числа в двоичном виде.
      */
+
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        if (value == 0) {
+            return String.valueOf(0);
+        } else {
+            while (value > 0) {
+                if (value % 2 == 0) {
+                    stringBuilder.append(value % 2);
+                } else {
+                    stringBuilder.append(1);
+                }
+                value = value / 2;
+            }
+        }
+        return stringBuilder.reverse().toString();
     }
 }
+
