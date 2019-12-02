94a048f6994bd6a86280a612ef5cab6e828987c1
Serhii Yevtushok <ggazila@gmail.com>
2019-12-02 17:54:59 +0200

    made small fix

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -11,13 +11,8 @@ public class BinaryString {
         String result = "0";
         if (value > 0) {
             while (value != 0) {
-                if (value % 2 == 0) {
-                    binaryString.append(0);
-                } else {
-                    binaryString.append(1);
-                }
+                binaryString.append(value % 2);
                 value = value / 2;
-
             }
             result = binaryString.reverse().toString();
         }
