295b8e2641c571c1e73292f521eed8e8515d69e2
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2019-12-03 14:57:04 +0200

    3 attempt

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class BinaryString {
     /**
      * Напишите метод toBinaryString(), который принимает на вход целое число value,
-     * а возвращает String с представлением этого числа в двоичном виде.
+     * а возврщает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
         StringBuffer sb = new StringBuffer("");
