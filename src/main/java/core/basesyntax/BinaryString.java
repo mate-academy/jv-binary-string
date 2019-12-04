f9d2feb8f70b92ee78bff5b923bc2047c9177c4e
levko <levkosvd@gmail.com>
2019-12-04 12:30:43 +0200

    hm3-v3 is fixed

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -5,21 +5,19 @@ public class BinaryString {
     /**
      * Напишите метод toBinaryString(), который принимает на вход целое число value,
      * а возвращает String с представлением этого числа в двоичном виде.
+     * @return
      */
     public String toBinaryString(int value) {
-        String num = "";
+        StringBuilder result = new StringBuilder();
         int b;
         if (value != 0) {
             while (value != 0) {
-                b = value % 2;
-                num = b + num;
+                result.append(value % 2);
                 value /= 2;
             }
         } else {
-            num = "0";
+            result.append(0);
         }
-        return num;
+        return result.reverse().toString();
     }
 }
-
-
