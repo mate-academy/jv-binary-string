4f0196f498741be6371a4017b9e7ac66f67e1e1c
Vinglish <khan.ildar94@gmail.com>
2020-03-30 16:10:51 +0300

    binary

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,17 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        if (value == 0) {
+            return "0";
+        }
+        for (;value != 0;) {
+            sb.append(value % 2);
+            value /= 2;
+        }
+
+        sb.reverse();
+        return sb.toString();
     }
 }
