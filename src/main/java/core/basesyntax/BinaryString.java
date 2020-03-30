ed2da2f088ddad1b04f54804ca0e29786798fdf7
Allduiin <vovapro01@gmail.com>
2020-03-30 17:51:05 +0300

    Added soution

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,14 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        if (value == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        while (value != 0) {
+            sb.insert(0,value % 2);
+            value /= 2;
+        }
+        return sb.toString();
     }
 }
