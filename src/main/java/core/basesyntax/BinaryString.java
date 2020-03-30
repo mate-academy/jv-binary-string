1ec62d3345d4d46de263964cc69a854b800a32ae
nastya <cctvporsh@gmail.com>
2020-03-30 16:40:01 +0300

    Edited some lines

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -8,9 +8,11 @@ public class BinaryString {
      */
     public String toBinaryString(int value) {
         StringBuilder str = new StringBuilder();
-        str.append(value == 0 ? "0" : "");
+        str.append(value == 0
+                ? "0" : "");
         while (value > 0) {
-            str.append((value % 2 == 0) ? "0" : "1");
+            str.append((value % 2 == 0)
+                    ? "0" : "1");
             value /= 2;
         }
         return str.reverse().toString();
