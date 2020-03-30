64611331b38ad84913724c3d60736e870a0ad5df
Polina Kulyk <polina.kulyk@protonmail.com>
2020-03-30 16:54:47 +0300

    Fixing CI

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -1,11 +1,6 @@
 package core.basesyntax;
 
 public class BinaryString {
-
-    /**
-     * Напишите метод toBinaryString(), который принимает на вход целое число value,
-     * а возвращает String с представлением этого числа в двоичном виде.
-     */
     public String toBinaryString(int value) {
         StringBuilder sb = new StringBuilder();
         for (int num = value; num > 0; num /= 2) {
