4768b01c70d29ff6e525f611ffb9583dc8981df5
Polina Kulyk <polina.kulyk@protonmail.com>
2020-03-30 17:03:55 +0300

    changed var names and eliminated code repetition

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -1,10 +1,15 @@
 package core.basesyntax;
 
 public class BinaryString {
+
+    /**
+     * Напишите метод toBinaryString(), который принимает на вход целое число value,
+     * а возвращает String с представлением этого числа в двоичном виде.
+     */
     public String toBinaryString(int value) {
         StringBuilder sb = new StringBuilder();
         for (int num = value; num > 0; num /= 2) {
-            sb.append(num%2);
+            sb.append(num % 2);
         }
         return (value == 0) ? String.valueOf(value) : sb.reverse().toString();
     }
