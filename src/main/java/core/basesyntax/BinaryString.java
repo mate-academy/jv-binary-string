175a07d8cab7b744679b6a27a9e8630d23adea22
Galina <gala6ap@mail.ru>
2020-03-30 16:41:15 +0300

    toBinaryString implementation added

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,11 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder("");
+        do {
+            stringBuilder.insert(0, value % 2);
+            value /= 2;
+        } while (value > 0);
+        return stringBuilder.toString();
     }
 }
