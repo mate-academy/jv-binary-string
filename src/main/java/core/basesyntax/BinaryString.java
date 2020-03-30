0f473d651de4d8982c382ede8d85fd0be884ea0f
Богдан Демьянчук <t0974447144@gmail.com>
2020-03-30 17:56:01 +0300

    took from exception value "1"

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,14 +7,14 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        if (value == 0 || value == 1) {
-            return Integer.toString(value);
+        if (value == 0) {
+            return "0";
         }
 
         StringBuilder builder = new StringBuilder();
 
-        while (value >= 1) {
-            builder.append( value % 2);
+        while (value > 0) {
+            builder.append(value % 2);
             value /= 2;
         }
         return builder.reverse().toString();
