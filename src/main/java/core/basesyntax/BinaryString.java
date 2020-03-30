6d4559f7a5dee7f84e86c3a281fb64670ff9f8c5
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-03-30 18:37:19 +0300

    solution added

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,19 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        if (value > 0) {
+            StringBuilder sb = new StringBuilder();
+
+            while (value > 0) {
+                if (value % 2 == 0) {
+                    sb.insert(0,"0");
+                } else {
+                    sb.insert(0,"1");
+                }
+                value = value / 2;
+            }
+            return sb.toString();
+        }
+        return "0";
     }
 }
