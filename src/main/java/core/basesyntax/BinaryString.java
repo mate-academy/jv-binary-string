a7c5c5f4c645db6560f1323b719173ffc161055e
andriy <maziarandriy@gmail.com>
2020-03-30 17:02:32 +0300

    hw1.3 fix solution

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,24 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return Integer.toBinaryString(value);
+        String result = "";
+        if (value == 0) {
+            return "0";
+        }
+        while (value > 0) {
+            if (value % 2 == 0) {
+                result = result + 0;
+                value = value / 2;
+            } else {
+                result = result + 1;
+                value = value / 2;
+            }
+        }
+        String[] sum = result.split("");
+        String fine = "";
+        for (int i = 1; i <= sum.length;i++) {
+            fine = fine + sum[sum.length - i];
+        }
+        return fine;
     }
 }
