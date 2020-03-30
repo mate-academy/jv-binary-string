1965e3565596041e704781343cc3e169616fd8ab
Andrey Tanchak <tanchak12@gmail.com>
2020-03-30 18:28:50 +0300

    added solution

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -2,11 +2,19 @@ package core.basesyntax;
 
 public class BinaryString {
 
-    /**
-     * Напишите метод toBinaryString(), который принимает на вход целое число value,
-     * а возвращает String с представлением этого числа в двоичном виде.
-     */
     public String toBinaryString(int value) {
-        return null;
+        if (value == 0) {
+            return "0";
+        }
+        String result = "";
+        while (value > 0) {
+            result = result + value % 2;
+            value = (value / 2);
+        }
+        StringBuilder itog = new StringBuilder(result);
+        itog = itog.reverse();
+        result = "" + itog;
+        return result;
+
     }
 }
