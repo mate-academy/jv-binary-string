7a74778cd4138ca7344075088b0fd208c7739ce8
Богдан Демьянчук <t0974447144@gmail.com>
2020-03-30 18:11:19 +0300

    Removed redundant empty lines

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -10,9 +10,7 @@ public class BinaryString {
         if (value == 0) {
             return "0";
         }
-
         StringBuilder builder = new StringBuilder();
-
         while (value > 0) {
             builder.append(value % 2);
             value /= 2;
