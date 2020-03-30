6f9c7906425d50f706224b782a1f8d969b25e4b3
VovkAA <vovkandrew@gmail.com>
2020-03-30 20:14:04 +0300

    Done

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,18 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder binaryValue = new StringBuilder();
+        if (value == 0) {
+            return "0";
+        } else if (value == 1) {
+            return "1";
+        }
+        while (value > 1) {
+            int remainder = value % 2;
+            binaryValue.append(remainder);
+            value = value / 2;
+        }
+        binaryValue.append("1");
+        return binaryValue.reverse().toString();
     }
 }
