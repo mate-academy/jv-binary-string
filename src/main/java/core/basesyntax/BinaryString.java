dda4f15de1670822321a4708ba331e7b6e7de4fe
unknown <bavikaalexey@gmail.com>
2020-03-30 16:34:18 +0300

    added solution

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -6,7 +6,19 @@ public class BinaryString {
      * Напишите метод toBinaryString(), который принимает на вход целое число value,
      * а возвращает String с представлением этого числа в двоичном виде.
      */
-    public String toBinaryString(int value) {
-        return null;
+
+    public static String toBinaryString(int value) {
+        int currentValue = value;
+        StringBuilder binaryValue = new StringBuilder();
+        int binaryRepresentation;
+
+        while (currentValue != 0) {
+            binaryRepresentation = currentValue % 2;
+            binaryValue.append(binaryRepresentation);
+            currentValue = currentValue / 2;
+        }
+        return value == 0 ? "0" : binaryValue.reverse().toString();
     }
 }
+
+
