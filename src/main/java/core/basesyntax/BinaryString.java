25e1699974debee50e1ff51575b7cf12b3349e32
Polina Kulyk <polina.kulyk@protonmail.com>
2020-03-30 18:38:42 +0300

    fixed indentation and simplified check for zero

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,7 +7,7 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        if (value < 1) {
+        if (value == 0) {
             return "0";
         }
         StringBuilder sb = new StringBuilder();
