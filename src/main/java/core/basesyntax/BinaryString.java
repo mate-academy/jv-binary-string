1a99453676fc8a8cca261ae89b6d1380fcad5d00
maks45 <maksim.durov45@gmail.com>
2020-03-30 18:20:46 +0300

    simplified extension in while loop

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -15,9 +15,10 @@ public class BinaryString {
         StringBuilder stringBuilder = new StringBuilder();
 
         while (value > 0) {
-            stringBuilder.append(value % 2 > 0 ? "1" : "0");
+            stringBuilder.append(value % 2);
             value = value / 2;
         }
+
         return stringBuilder.reverse().toString();
     }
 }
