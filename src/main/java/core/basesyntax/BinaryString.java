35672bd2ebbf6b2f46b326a50ca4e8404f7742fd
ana-dav <opa.super.op@gmail.com>
2020-03-30 18:54:16 +0300

    var1

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,17 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+
+        if (value == 0) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (value != 0) {
+            sb.append(value % 2);
+            value /= 2;
+        }
+        return sb.reverse().toString();
     }
 }
