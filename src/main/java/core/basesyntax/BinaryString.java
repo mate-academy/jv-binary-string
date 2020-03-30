1acf976b5376d0b155525cd99cb6d3ea9824e5f3
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-03-30 17:46:55 +0300

    added solution for homework 1.3

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,15 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder answer = new StringBuilder();
+        do {
+            if (value % 2 == 0) {
+                answer.append('0');
+            } else {
+                answer.append('1');
+            }
+            value /= 2;
+        } while (value != 0);
+        return answer.reverse().toString();
     }
 }
