c825bcf3959666ede2ee654ddeefc3b89909f2e5
Maryna <kaijou88@gmail.com>
2020-03-31 11:57:59 +0300

    deleted variable and else statement

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -8,18 +8,15 @@ public class BinaryString {
      */
     public String toBinaryString(int value) {
         StringBuilder binary = new StringBuilder();
-        int remainder;
         if (value == 0 | value == 1) {
-            binary.append(value);
-        } else {
-            while (value > 1) {
-                remainder = value % 2;
-                binary.append(remainder);
-                value = (value - remainder) / 2;
-            }
-            binary.append(value);
-            binary.reverse();
+            return new String(binary.append(value));
         }
+        while (value > 1) {
+            binary.append(value % 2);
+            value = (value - value % 2) / 2;
+        }
+        binary.append(value);
+        binary.reverse();
         return new String(binary);
     }
 }
