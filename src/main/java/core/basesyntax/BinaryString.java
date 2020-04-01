404188117b75235825321e40cc650cac6928cc1e
Oleksandr Kovaliuk <kovaliukoleksandr@gmail.com>
2020-04-01 09:14:28 +0300

    toBinaryString in BinaryString class

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,15 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        if (value == 0) {
+            return "0";
+        }
+        while (value != 0) {
+            sb.append(value % 2);
+            value /= 2;
+        }
+        return sb.reverse().toString();
     }
 }
