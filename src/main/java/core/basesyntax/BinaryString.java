998948d2faa6fad00b812a839bd8d082974b5da7
ROMASHSX <andriiromash@gmail.com>
2020-07-27 14:23:06 +0300

    Task is redone

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -3,6 +3,14 @@ package core.basesyntax;
 public class BinaryString {
 
     public String toBinaryString(int value) {
-        return Integer.toBinaryString(value);
+        if(value == 0) {
+            return "0";
+        }
+        StringBuilder binaryString = new StringBuilder();
+        while (value >=1 ) {
+            binaryString.append(value % 2);
+            value /= 2;
+        }
+        return binaryString.reverse().toString();
     }
 }
