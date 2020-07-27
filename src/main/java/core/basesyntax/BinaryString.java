4d4e1b6efbf57abe1cba413242a6c4832f2c7d1e
ROMASHSX <andriiromash@gmail.com>
2020-07-27 14:30:15 +0300

    Task is redone and maven is checked

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class BinaryString {
 
     public String toBinaryString(int value) {
-        if(value == 0) {
+        if (value == 0) {
             return "0";
         }
         StringBuilder binaryString = new StringBuilder();
-        while (value >=1 ) {
+        while (value >= 1) {
             binaryString.append(value % 2);
             value /= 2;
         }
