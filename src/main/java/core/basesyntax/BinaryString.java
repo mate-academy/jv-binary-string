5a62fec1a61b2bab7fef58c4015476896b17d5d9
Oleksii Tromsa <tromsaalex@gmail.com>
2020-07-27 16:59:43 +0300

    add solution

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -1,12 +1,16 @@
 package core.basesyntax;
 
 public class BinaryString {
-
-    /**
-     * Напишите метод toBinaryString(), который принимает на вход целое число value,
-     * а возвращает String с представлением этого числа в двоичном виде.
-     */
     public String toBinaryString(int value) {
-        return null;
+        if (value == 0) {
+            return "0";
+        }
+
+        StringBuffer result = new StringBuffer();
+        while (value > 0) {
+            result.insert(0, value % 2);
+            value /= 2;
+        }
+        return result.toString();
     }
 }
