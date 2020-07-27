75ac1ce5168439e1c0921592b767fb4435df9ba1
Marina <maryna.voloshyna.11@gmail.com>
2020-07-27 18:53:47 +0300

    hm1.3

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,26 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        String s = "";
+        int a = 0;
+        int k = value;
+        while (k / 2 != 0) {
+            a++;
+            k = k / 2;
+        }
+        a++;
+        int[] arr = new int[a];
+        int i = a - 1;
+        while (value / 2 != 0) {
+            arr[i] = value % 2;
+            value = value / 2;
+            i--;
+        }
+        arr[i] = value;
+        for (int j = 0; j < arr.length; j++) {
+            s = s + Integer.toString(arr[j]);
+        }
+
+        return s;
     }
 }
