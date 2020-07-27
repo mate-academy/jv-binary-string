5735a5661ac55fc29014b336e7b1ffa4582bab45
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-07-27 16:16:51 +0300

    Add solution

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,25 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        if (value == 0) {
+            return "0";
+        }
+        StringBuilder result = new StringBuilder();
+        int power = 0;
+        while (Math.pow(2, power) <= value) {
+            power++;
+        }
+        power--;
+        while (power >= 0) {
+            if (Math.pow(2, power) > value) {
+                result.append("0");
+                power--;
+            } else {
+                value -= Math.pow(2, power);
+                power--;
+                result.append("1");
+            }
+        }
+        return result.toString();
     }
 }
