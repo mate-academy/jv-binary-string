0569ddcf6d9debb6b4f5c8ce686b2111a99f8045
Leonid <rommel@ukr.net>
2020-07-27 12:27:55 +0300

    hw-3 done

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,20 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        if (value == 0) {
+            return "0";
+        }
+        boolean isNegative = value < 0;
+        if (isNegative) {
+            value = -value;
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = value; i > 0; i /= 2) {
+            result.append(i % 2);
+        }
+        if (isNegative) {
+            result.append("-");
+        }
+        return result.reverse().toString();
     }
 }
