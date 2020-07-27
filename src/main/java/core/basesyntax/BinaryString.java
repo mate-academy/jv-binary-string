89ab7ea5da6e476e85180b3da89a4ed41a1ab1be
TimoxaV <vadim2493@gmail.com>
2020-07-27 18:20:52 +0300

    fixed solution 1.3 - renamed variable

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,15 +7,15 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        StringBuilder res = new StringBuilder("");
+        StringBuilder resultBinary = new StringBuilder();
         if (value < 2) {
-            res.append(value);
+            resultBinary.append(value);
         } else {
             while (value > 0) {
-                res.insert(0,value % 2);
+                resultBinary.insert(0,value % 2);
                 value = value / 2;
             }
         }
-        return res.toString();
+        return resultBinary.toString();
     }
 }
