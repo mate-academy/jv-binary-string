62cb85924ee6931c2fe8d6c06ecf2bf4d7205505
Borisko130 <krimenyukb@gmail.com>
2020-07-27 18:18:23 +0300

    Homework 3 completed

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,17 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder binaryResult = new StringBuilder();
+
+        if (value == 0) {
+            return "0";
+        }
+
+        for (int i = 0; value > 0; i++) {
+            binaryResult.append(((value % 2) == 0 ? "0" : "1"));
+            value /= 2;
+        }
+
+        return binaryResult.reverse().toString();
     }
 }
