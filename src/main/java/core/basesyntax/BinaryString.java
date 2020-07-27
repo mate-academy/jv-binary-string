e98792f3f6dd82fb4601efaad5c97a8ab92c4c81
m.mikhajlov <lizappy31@gmail.com>
2020-07-27 18:53:06 +0300

    hw-1.3

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,15 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        if (value == 0) {
+            return "0";
+        }
+        String result = "";
+        while (value > 0) {
+            int a = value % 2;
+            result = a + result;
+            value = value / 2;
+        }
+        return result;
     }
 }
