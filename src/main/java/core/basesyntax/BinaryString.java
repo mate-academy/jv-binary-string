4bcbae320113ac8b73baac5335b2317d360557db
Alexey <gradiviro@gmail.com>
2020-07-27 18:09:26 +0300

    third solution added

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,11 +7,11 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        if (value == 0){
+        if (value == 0) {
             return "0";
         }
         StringBuilder binaryString = new StringBuilder();
-        while (value >= 1){
+        while (value >= 1) {
             binaryString.append(value % 2);
             value /= 2;
         }
