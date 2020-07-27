ee0570f94637ed0b04c6d360b49f57ff6056a2b9
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-07-27 17:38:42 +0300

    hw-3

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,15 +7,16 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        if (value == 0) return "0";
+        if (value == 0) {
+            return "0";
+        }
         StringBuilder builder = new StringBuilder("");
         int remainder = 0;
-        while(value >= 1) {
-            if(value == 1) {
+        while (value >= 1) {
+            if (value == 1) {
                 builder.append(1);
                 break;
-            }
-            else {
+            } else {
                 remainder = value % 2;
                 builder.append(remainder);
                 value = Math.round(value / 2);
