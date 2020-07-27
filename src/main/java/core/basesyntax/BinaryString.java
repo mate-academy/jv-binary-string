a527a7637af0a94b28439c1bc5f5227117788d32
Ilya <ilya03449@gmail.com>
2020-07-27 18:47:22 +0300

    fixed

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,12 +7,12 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        if (value == 1 || value == 0) {
-            return "" + value;
+        if (value == 0) {
+            return "0";
         }
         StringBuilder resultStr = new StringBuilder();
         for (int i = value; i > 0; i /= 2) {
-            resultStr.append(i % 2 == 0 ? 0 : 1);
+            resultStr.append(i % 2);
         }
 
         return resultStr.reverse().toString();
