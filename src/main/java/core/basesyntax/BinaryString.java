100135240a0a1caedbd6d6964d086ddda1c2af72
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-07-27 20:56:34 +0300

    task 1.3 Now with correct indentations!

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -2,12 +2,13 @@ package core.basesyntax;
 
 public class BinaryString {
     public String toBinaryString(int value) {
-        if (value == 0) {return "0";}
+        if (value == 0) {
+            return "0";
+        }
         StringBuilder valueSB = new StringBuilder();
-        while (value != 0)
-        {
-            valueSB.append((value%2));
-            value = value/2;
+        while (value != 0) {
+            valueSB.append((value % 2));
+            value = value / 2;
         }
         return valueSB.reverse().toString();
     }
