ec9b0ca85a3968d57af4698bba6361c03910ce9f
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-07-27 17:49:12 +0300

    empty lines and redundant variable  removed

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -3,22 +3,16 @@ package core.basesyntax;
 public class BinaryString {
 
     public String toBinaryString(int value) {
-
         StringBuilder binaryStr = new StringBuilder();
         int res = 0;
-
         if (value == 0) {
             return "0";
         }
-
         while (value != 0) {
             res = value % 2;
             binaryStr.append(res);
             value = value / 2;
         }
-
-        String binary = binaryStr.reverse().toString();
-
-        return binary;
+        return binaryStr.reverse().toString();
     }
 }
