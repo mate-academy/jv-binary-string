60f3c057239fa47bf67db216838373e8e3d58e33
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-07-28 13:03:53 +0300

    hw-3

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -10,10 +10,10 @@ public class BinaryString {
         if (value == 0) {
             return "0";
         }
-        StringBuilder builder = new StringBuilder("");
-        while (value > 0.5) {
+        StringBuilder builder = new StringBuilder();
+        while (value > 0) {
             builder.append(value % 2);
-            value = Math.round(value / 2);
+            value = value / 2;
         }
         return builder.reverse().toString();
     }
