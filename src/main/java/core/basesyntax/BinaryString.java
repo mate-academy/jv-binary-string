4a27ae96c8624c4bc7716cd200688234579d4d5b
romanHreits <romagreic@gmail.com>
2020-07-28 12:13:57 +0300

    aded solution binary-string hw-1.3

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -7,6 +7,15 @@ public class BinaryString {
      * а возвращает String с представлением этого числа в двоичном виде.
      */
     public String toBinaryString(int value) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        if (value > 1) {
+            while (value > 0) {
+                int s = !(value % 2 == 0) ? 1 : 0;
+                value /= 2;
+                stringBuilder.append(s);
+            }
+            return stringBuilder.reverse().toString();
+        }
+        return String.valueOf(value);
     }
 }
