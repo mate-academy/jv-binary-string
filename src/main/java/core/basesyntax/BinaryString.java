10a7e0caf3a1d9952140c7fd926f8a93a1172629
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-07-28 12:22:47 +0300

    rename valuable and fixed .gitignore file

--- src/main/java/core/basesyntax/BinaryString.java
+++ src/main/java/core/basesyntax/BinaryString.java
@@ -10,11 +10,11 @@ public class BinaryString {
         if (value == 0) {
             return "0";
         }
-        StringBuilder isBinary = new StringBuilder();
+        StringBuilder binaryString = new StringBuilder();
         while (value > 0) {
-            isBinary.append(value % 2);
+            binaryString.append(value % 2);
             value /= 2;
         }
-        return isBinary.reverse().toString();
+        return binaryString.reverse().toString();
     }
 }
